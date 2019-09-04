ba161832252989df07aa9d317841822abdd2bf7d
MolyGitHub <47333908+MolyGitHub@users.noreply.github.com>
2019-09-04 12:44:49 +0200

    changed port

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
