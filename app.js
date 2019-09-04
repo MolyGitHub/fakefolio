79edd6c29ff7942d4347b578fcfd657b44c6e4b5
MolyGitHub <47333908+MolyGitHub@users.noreply.github.com>
2019-09-04 13:38:16 +0200

    Change hello to Javascript

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Javascript!'))
 
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
